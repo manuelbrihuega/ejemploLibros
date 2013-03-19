ddd8576c425e07802b341ea70ba4924f026d0a55
Manuel Jesús de la Calle Brihuega <manuel.brihuega@gmail.com>
2013-03-19 21:10:26 +0100

    Añadida la validación para ISBN13 en el formulario

--- WebContent/js/validacion.js
+++ WebContent/js/validacion.js
@@ -1,10 +1,30 @@
+window.onload=generarEvento;
+
 function validacion(){
 	var isbn= document.getElementById("isbn");
 	var miformulario=document.getElementById("miformulario");
 	if(isbn.value==""){
-		alert("datos no validos");
+		alert("Introduce el ISBN");
 		return false;
 	}else{
-		miformulario.submit();
+		if(isbn.value.length!=17){
+			alert("ISBN incompleto");
+			return false;
+		}else{
+			if(isbn.value.charAt(3)=='-' && isbn.value.charAt(6)=='-' && isbn.value.charAt(11)=='-' && isbn.value.charAt(15)=='-'){
+				miformulario.submit();
+			}else{
+				alert("ISBN incorrecto");
+				return false;
+			}
+		}
 	}
+}
+
+function generarEvento() {
+	var formularioInsercion= document.getElementById("formularioInsercion");
+	formularioInsercion.onsubmit= function () {
+		 validacion();
+		 return false;
+	};
 }
